67f387bfccf80776c67e1dd9f12f4dec3436ad3b
MockBL <mockbl@proton.me>
2024-01-31 00:29:04 +0800

    mint first done

--- dot20.py
+++ dot20.py
@@ -7,13 +7,13 @@ from dotadb.db import DotaDB
 class Dot20:
 
     def __init__(self, db_url: str):
-        self.dota_db = DotaDB(db_url=db_url)
+        self.dota_db = DotaDB(db_url)
         self.p = "dot-20"
 
     # 部署
     def deploy(self, **json_data):
         try:
-            (json, memo) = self._fmt_json_data(op="deploy", **json_data)
+            (json, memo) = self._fmt_json_data("deploy", **json_data)
         except Exception as e:
             raise e
 
@@ -24,7 +24,7 @@ class Dot20:
             raise Exception("tick is empty")
         elif not re.match(tick_pattern, tick):
             raise Exception("3-6 letters are case insensitive")
-        elif self.get_deploy_info(tick=tick) is not None:
+        elif self.get_deploy_info(tick) is not None:
             raise Exception(f"'{tick}' already exist")
 
         # 验证decimal 数字0-18
@@ -32,57 +32,61 @@ class Dot20:
         if decimal is None:
             memo["decimal"] = 18
         elif not (0 <= decimal <= 18):
-            raise Exception("仅允许数字0-18")
+            raise Exception("Only digits 0-18 are allowed")
 
         # 获取模式
         mode = memo.get("mode")
         if mode is None:
-            raise Exception("模式读取失败")
+            raise Exception("Mode get failure")
         if not mode in ["normal", "fair", "owner"]:
-            raise Exception("未知模式")
+            raise Exception("Unknown mode")
 
         # 判断start与区块高度
         start = memo.get("start")
         if start is None:
-            raise Exception("读取start失败")
+            raise Exception("Start get failure")
         block_num = json.get("block_num")
         if block_num is None:
-            raise Exception("读取区块失败")
+            raise Exception("Block get failure")
         if start < block_num:
-            raise Exception("start必须等于或晚于部署区块高度")
+            raise Exception(
+                "start must be equal to or later than the deployment block height")
 
         # 常规模式（normal，单次mint获得固定数量铭文）
         # <start, max, lim>
         if mode == "normal":
             max, lim = (memo.get(key) for key in ['max', 'lim'])
             if max is None or lim is None:
-                raise Exception("常规模式:max或lim读取失败")
+                raise Exception("The max or lim read failed")
             if lim == 0:
-                raise Exception("normal: lim is 0")
+                raise Exception("lim is 0")
             if max == 0:
-                raise Exception("normal: max is 0")
+                raise Exception("max is 0")
             if max > 10 ** 32:
-                raise Exception("常规模式:max最大值为10^32")
+                raise Exception("max Max is 10^32")
             if lim > max:
-                raise Exception("常规模式:单次lim铸造数量不能高于max")
+                raise Exception(
+                    "The amount of lim cast at a time cannot be higher than max")
 
         # 公平模式（fair，区块内平分铭文）
         # <amt, start, end>
         if mode == "fair":
             amt, end = (memo.get(key) for key in ['amt', 'end'])
             if amt is None or end is None:
-                raise Exception("公平模式:amt或end读取失败")
+                raise Exception("amt or end read failed")
             if amt*(end-start+1) > 10 ** 32:
-                raise Exception("公平模式:amt*(end-start+1)不得高于10^32")
+                raise Exception(
+                    "amt*(end-start+1) must not be higher than 10^32")
 
         # 控制者模式（owner，仅admin可以铸造铭文）
         # <admin>
         if mode == "owner":
             admin = memo.get("admin")
             if admin is None:
-                raise Exception("控制者模式:admin读取失败")
+                raise Exception("admin read failed")
             if self.is_valid_ss58_address(admin) is False:
-                raise Exception("控制者模式:admin不符合ss58规范")
+                raise Exception(
+                    "admin does not comply with the ss58 specifications")
 
         deploy_data = {
             "deployer": json.get("user"),
@@ -94,18 +98,15 @@ class Dot20:
             **memo
         }
         try:
-            self.dota_db.create_tables_for_new_tick(tick=tick)
+            self.dota_db.create_tables_for_new_tick(tick)
             self.dota_db.insert_deploy_info(deploy_data)
         except Exception as e:
-            print("========================")
-            print(f"{e}")
-            print("========================")
             raise e
 
     # 铸造
-    def mint(self, **json_data) -> dict:
+    def mint(self, **json_data):
         try:
-            (json, memo) = self._fmt_json_data(op="mint", **json_data)
+            (json, memo) = self._fmt_json_data("mint", **json_data)
         except Exception as e:
             raise e
 
@@ -113,27 +114,54 @@ class Dot20:
         tick = memo.get("tick")
         if tick is None:
             raise Exception("tick is empty")
-        deploy_info = self.get_deploy_info(tick=tick)
+        deploy_info = self.get_deploy_info(tick)
         if deploy_info is None:
             raise Exception(f"{tick}'s deploy does not exist")
 
         # mint是否结束
         block_num = json.get("block_num")
         if block_num is None:
-            raise Exception("读取区块失败")
-        if self.is_mint_finish(block_num=block_num, **deploy_info) is True:
-            raise Exception("Mint已结束")
+            raise Exception("Block read failure")
+        if self.is_mint_finish(block_num, **deploy_info) is True:
+            raise Exception("Mint has ended")
 
         # to是否符合ss58
         to = memo.get("to")
         if to is None:
-            raise Exception("to字段读取失败")
+            raise Exception("to read failed")
         if self.is_valid_ss58_address(to) is False:
-            raise Exception("地址不符合ss58规范")
+            raise Exception(
+                "The address does not comply with the ss58 specification")
 
-        # TODO:normal/fair
+        # 获取模式
+        mode = deploy_info.get("mode")
+        if mode is None:
+            raise Exception("Mode get failure")
+        if not mode in ["normal", "fair", "owner"]:
+            raise Exception("Unknown mode")
 
-        # TODO:owner
+        # normal
+        if mode == "normal":
+            lim = memo.get("lim")
+            if lim is None:
+                raise Exception("lim read failed")
+            deploy_lim = deploy_info.get("lim")
+            if deploy_lim is None:
+                raise Exception("deploy's lim read failed")
+            if lim > deploy_lim:
+                raise Exception(
+                    "The quantity must not be higher than a single mint quantity")
+
+        # owner
+        if mode == "owner":
+            user = json.get("user")
+            if user is None:
+                raise Exception("user read failed")
+            deploy_owner = deploy_info.get("admin")
+            if deploy_owner is None:
+                raise Exception("deploy's owner read failed")
+            if user != deploy_owner:
+                raise Exception("Only the owner address can be mint")
 
         mint_data = {
             "singer": json.get("user"),
@@ -145,7 +173,12 @@ class Dot20:
             **memo
         }
         try:
-            self.dota_db.insert_mint_info(tick=tick, mint_infos=[mint_data])
+            self.dota_db.insert_mint_info(tick, [mint_data])
+            user_currency_balance = self.get_user_currency_balance(
+                tick, json.get("user"))
+            if user_currency_balance is not None:
+                self.dota_db.insert_or_update_user_currency_balance(
+                    tick, user_currency_balance)
         except Exception as e:
             raise e
 
@@ -158,30 +191,50 @@ class Dot20:
             return False
 
     # 获取deploy_info，并判断tick是否存在
-    def get_deploy_info(self, tick: str) -> None | dict:
-        # 从数据库获取deploy信息
+    def get_deploy_info(self, tick: str):
         try:
-            res = self.dota_db.get_deploy_info(tick=tick)
-            if len(res) == 0:
+            tbl_result = self.dota_db.get_deploy_info(tick)
+            if len(tbl_result) == 0:
                 return None
             else:
-                print(f"[999]:{res[0]}")
-                return res[0]
-        except Exception as e:
+                return tbl_result[0]
+        except Exception:
+            return None
+
+    # 获取get_total_supply,已mint总量
+    def get_total_supply(self, tick: str):
+        try:
+            tbl_result = self.dota_db.get_total_supply(tick)
+            if len(tbl_result) == 0:
+                return None
+            else:
+                return tbl_result[0]
+        except Exception:
+            return None
+
+    # 获取用户余额
+    def get_user_currency_balance(self, tick: str, user: str):
+        try:
+            tbl_result = self.dota_db.get_user_currency_balance(
+                tick, user)
+            if len(tbl_result) == 0:
+                return None
+            else:
+                return tbl_result[0]
+        except Exception:
             return None
 
     # 验证mint是否结束
     def is_mint_finish(self, block_num, **deploy_info) -> bool:
-        mode = deploy_info.get("mode")
+        mode, max, tick = (deploy_info.get(key)
+                           for key in ['mode', 'max', 'tick'])
         if mode == "normal":
-            max = deploy_info.get("max")
-            # TODO:获取mint余量
-            residual = 100000
-            if max is not None and residual > 0:
+            total = self.get_total_supply(tick)
+            if max is not None and total is not None and total > max:
                 return True
         if mode == "fair":
             end = deploy_info.get("end")
-            if end is not None and block_num is not None and end > block_num:
+            if end is not None and block_num is not None and block_num > end:
                 return True
 
         return False
@@ -195,10 +248,10 @@ class Dot20:
 
         memo = data.get("memo")
         if memo is None:
-            raise Exception("memo获取失败")
+            raise Exception("Failed to obtain memo")
         if memo.get("p") != self.p:
-            raise Exception("非dot-20数据")
+            raise Exception("Non dot-20 data")
         if memo.get("op") != op:
-            raise Exception(f"请传入'{op}'的json数据")
+            raise Exception(f"Please pass in the json data for '{op}'")
 
         return data, memo
--- test_dot20.py
+++ test_dot20.py
@@ -5,13 +5,31 @@ from dot20 import Dot20
 class TestDot20(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         self.dot20 = Dot20(
-            db_url="mysql+pymysql://root:12345678@localhost:3306/wjy")
+            db_url="mysql+mysqlconnector://root:123456@localhost:3306/wjy")
         super().__init__(methodName)
 
     def test_deploy(self):
         try:
 
             deploy_json = {
+                "block_num": 273111,
+                "block_hash": "0x240079607dbb76c81b974be2256fbf79ed809995a973e1b3b1292c6b5ec4d7d0",
+                "extrinsic_hash": "0x9cef5f083d7ed72098bfe6768d65602d8fa196c696bcf3456a4e5a982e45aa7a",
+                "extrinsic_index": 2,
+                "batchall_index": 0,
+                "remark_index": 0,
+                "remark_hash": "0x98f4b6890ae25bb9dd975a50f320fc1ab0cfbbd92673a55c9fc58ffac25aedfb",
+                "origin": "5FTcboVf86hubC8YJjo8LjK3c2uq2rWpK7idnrfazi4ePuZy",
+                "user": "5FTcboVf86hubC8YJjo8LjK3c2uq2rWpK7idnrfazi4ePuZy",
+                "memo": "{\"p\": \"dot-20\", \"op\": \"deploy\",\"mode\": \"fair\", \"tick\": \"dota\",\"decimal\": 18, \"start\": 273115, \"max\": 10000000000, \"lim\": 1000000, \"amt\": 1000, \"end\": 283115, \"admin\": \"15Dfr41oVWMhqKD6qL1pj6gZEE6vzNQ9RH8ShefFsoReVWsi\"}"
+            }
+            self.dot20.deploy(**deploy_json)
+        except Exception as e:
+            print(f"DEPLOY_ERR::{e}\n")
+
+    def test_mint(self):
+        try:
+            mint_json = {
                 "block_num": 273115,
                 "block_hash": "0x240079607dbb76c81b974be2256fbf79ed809995a973e1b3b1292c6b5ec4d7d0",
                 "extrinsic_hash": "0x9cef5f083d7ed72098bfe6768d65602d8fa196c696bcf3456a4e5a982e45aa7a",
@@ -21,29 +39,11 @@ class TestDot20(unittest.TestCase):
                 "remark_hash": "0x98f4b6890ae25bb9dd975a50f320fc1ab0cfbbd92673a55c9fc58ffac25aedfb",
                 "origin": "5FTcboVf86hubC8YJjo8LjK3c2uq2rWpK7idnrfazi4ePuZy",
                 "user": "5FTcboVf86hubC8YJjo8LjK3c2uq2rWpK7idnrfazi4ePuZy",
-                "memo": "{\"p\": \"dot-20\", \"op\": \"deploy\",\"mode\": \"owner\", \"tick\": \"dota\",\"decimal\": 12, \"start\": 273115, \"max\": 1000000000000000000000, \"lim\": 1000000000000000, \"amt\": 1000000000000, \"end\": 123444, \"admin\": \"15Dfr41oVWMhqKD6qL1pj6gZEE6vzNQ9RH8ShefFsoReVWsi\"}"
+                "memo": "{\"p\": \"dot-20\", \"op\": \"mint\",\"tick\": \"dota\", \"lim\": 1000000000000000000000, \"to\": \"15Dfr41oVWMhqKD6qL1pj6gZEE6vzNQ9RH8ShefFsoReVWsi\"}"
             }
-            self.dot20.deploy(**deploy_json)
+            self.dot20.mint(**mint_json)
         except Exception as e:
-            print(f"DEPLOY_ERR::{e}\n")
-
-    # def test_mint(self):
-    #     try:
-    #         mint_json = {
-    #             "block_num": 273115,
-    #             "block_hash": "0x240079607dbb76c81b974be2256fbf79ed809995a973e1b3b1292c6b5ec4d7d0",
-    #             "extrinsic_hash": "0x9cef5f083d7ed72098bfe6768d65602d8fa196c696bcf3456a4e5a982e45aa7a",
-    #             "extrinsic_index": 2,
-    #             "batchall_index": 0,
-    #             "remark_index": 0,
-    #             "remark_hash": "0x98f4b6890ae25bb9dd975a50f320fc1ab0cfbbd92673a55c9fc58ffac25aedfb",
-    #             "origin": "5FTcboVf86hubC8YJjo8LjK3c2uq2rWpK7idnrfazi4ePuZy",
-    #             "user": "5FTcboVf86hubC8YJjo8LjK3c2uq2rWpK7idnrfazi4ePuZy",
-    #             "memo": "{\"p\": \"dot-20\", \"op\": \"mint\",\"tick\": \"dota\", \"lim\": 1000000000000000000000, \"to\": \"15Dfr41oVWMhqKD6qL1pj6gZEE6vzNQ9RH8ShefFsoReVWsi\"}"
-    #         }
-    #         self.dot20.mint(**mint_json)
-    #     except Exception as e:
-    #         print(f"MINT_ERR::{e}\n")
+            print(f"MINT_ERR::{e}\n")
 
 
 if __name__ == '__main__':
