8331d16d7606c4c7bd12727ed2af913f3c4944d9
YanOctavian <iu4ryhiuenb@proton.me>
2024-01-31 01:27:47 +0800

    更新init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dot20',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'base58==2.1.1',
