acd1fab0b24452007ae469363254f0961e06c0c1
MockBL <mockbl@proton.me>
2024-02-01 14:08:31 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dot20',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'base58==2.1.1',
